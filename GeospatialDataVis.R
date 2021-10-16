8945a45fe6c697ecbdab111af862e3dcde657ffc
bugn200 <bugn200@aucklanduni.ac.nz>
2021-10-16 16:06:02 +1300

    Update delimiter and clean up visualisation code

--- GeospatialDataVis.R
+++ GeospatialDataVis.R
@@ -7,6 +7,7 @@ library(OpenStreetMap)
 library(raster)
 library(rgdal)
 library(osrm)
+source('GeospatialFunctions.R')
 #Read Supermarket location data from the csv
 supermarket_raw=read.csv('data/WoolworthsRegion.csv')
 #Convert Longitude and Latitude to be a geometry, using EPSG 4326 system
@@ -21,6 +22,7 @@ nz_shape=st_read("Shape_files/TA_2018_clipped")
 akl_shape=nz_shape%>%filter(TA2018_V_1=="Auckland")
 #Crop the map for the area of interest
 akl_shape=st_crop(akl_shape,xmin=1740000,xmax=1781000,ymin=5890000,ymax=5940000)
+akl_shape1=st_transform(akl_shape,4326)
 #Initially visualise data for weekdays
 data_vis=tm_shape(akl_shape)+tm_polygons()+tm_shape(supermarket)+tm_dots(col='Type',palette='Dark2',size=0.3)+ tm_layout(legend.position = c("right", "top"), 
                                                                                                                          title= 'Woolworth supermarkets location', 
@@ -48,75 +50,12 @@ East_shape=st_crop(akl_shape,xmin=1759000,xmax=1773000,ymin=5906800,ymax=5918500
 West=filter(supermarket1,Region%in%c('West','Distribution Centre Auckland'))
 West_shape=st_crop(akl_shape,xmin=1741100,xmax=1762000,ymin=5908000,ymax=5928000)
 
-
-
-#Read the route file for weekday route and save as a png
-weekdayRoute=readLines('data/WeekdayRouteStores.txt')
-#Create a matrix to save data
-tripMatrix=matrix(0,65,4)
-colnames(tripMatrix)=c('Long','Lat','LongEnd','LatEnd')
-#Initialise for the number of node(will be used to plot)
-nodeReach=1
-for(i in 1:length(weekdayRoute)) {
-  #Initialise start point and split the string
-  route=as.vector(st_coordinates(filter(supermarket,Store=='Distribution Centre Auckland')$geometry))
-  weekdayR=strsplit(weekdayRoute[i],split=',')
-  for(j in 1:length(weekdayR[[1]])) {
-    #Add coordinate to the start and the end point of the arrows
-    tripMatrix[,'Long'][nodeReach]=route[1]
-    tripMatrix[,'Lat'][nodeReach]=route[2]
-    #Add coordinate to the end point of arrows
-    route1=as.vector(st_coordinates(filter(supermarket,Store==weekdayR[[1]][j])$geometry))
-    tripMatrix[,'LongEnd'][nodeReach]=route1[1]
-    tripMatrix[,'LatEnd'][nodeReach]=route1[2]
-    route=route1
-    nodeReach=nodeReach+1
-  }
-}
-#change tripMatrix to a dataframe
-tripMatrix=as.data.frame(tripMatrix)
-#Plot the route and save as png
-ggplot(akl_shape)+
-  geom_sf(data=akl_shape,fill='grey')+
-  geom_sf(data=supermarket,aes(color=Type))+
-  scale_color_brewer(palette="Dark2")+
-  geom_segment(data=tripMatrix,aes(x=Long,y=Lat,xend=LongEnd,yend=LatEnd),
-               arrow=arrow(length=unit(0.2,'cm')),alpha=0.3)+
-  labs(title='Weekday Routes')
+weekdaymap=arrowRoute(routeFilename = 'data/WeekdayRouteStores.txt',storeDf = supermarket)
+weekdaymap
 ggsave('Plot/WeekdayRoute.png')
 #Read the route file for Saturday route and save as a png
-saturdayRoute=readLines('data/SaturdayRouteStores.txt')
-#Create a matrix to save the data
-satTrip=matrix(0,53,4)
-colnames(satTrip)=c('Long','Lat','LongEnd','LatEnd')
-#Initialise number of node Reach
-nodeReach=1
-for(i in 1:length(saturdayRoute)) {
-  #Initialise the start point and split the string
-  route=as.vector(st_coordinates(filter(supermarket,Store=='Distribution Centre Auckland')$geometry))
-  satRoute=strsplit(saturdayRoute[i],split=',')
-  for(j in 1:length(satRoute[[1]])) {
-    #Add the start point to the matrix
-    satTrip[,'Long'][nodeReach]=route[1]
-    satTrip[,'Lat'][nodeReach]=route[2]
-    #Add the end point coordinate to the matrix
-    route1=as.vector(st_coordinates(filter(supermarket,Store==satRoute[[1]][j])$geometry))
-    satTrip[,'LongEnd'][nodeReach]=route1[1]
-    satTrip[,'LatEnd'][nodeReach]=route1[2]
-    route=route1
-    nodeReach=nodeReach+1
-  }
-}
-#Convert satTrip to a data frame
-satTrip=as.data.frame(satTrip)
-#Plot the map and save it as a png
-ggplot(akl_shape)+
-  geom_sf(data=akl_shape,fill='grey')+
-  geom_sf(data=saturday,aes(color=Type))+
-  scale_color_brewer(palette="Dark2")+
-  geom_segment(data=satTrip,aes(x=Long,y=Lat,xend=LongEnd,yend=LatEnd),
-               arrow=arrow(length=unit(0.2,'cm')),alpha=0.3)+
-  labs(title='Saturday Routes')
+satmap=arrowRoute(routeFilename = 'data/SaturdayRouteStores.txt',storeDf= saturday)
+satmap
 ggsave('Plot/SaturdayRoute.png')
 #Transform all the region shape to 4326 to plot the regional route map
 North_shape=st_transform(North_shape,4326)
@@ -125,24 +64,9 @@ East_shape=st_transform(East_shape,4326)
 West_shape=st_transform(West_shape,4326)
 Central_shape=st_transform(Central_shape,4326)
 #Classify region for weekdayRoute
-w=as.data.frame(weekdayRoute)
-colnames(w)='Route'
-w$Region='Unknown'
-#classify region and the route it is in
-for(i in 1:nrow(w)) {
-  #split the string for the route
-  route=str_split(w[i,1],',')
-  w[i,2]=filter(supermarket,Store==route[[1]][1])$Region
-}
+w=regionClassifier(routeFilename = 'data/WeekdayRouteStores.txt',storeDf = supermarket)
 #Do the same thing for Saturday routes
-s=as.data.frame(saturdayRoute)
-colnames(s)='Route'
-s$Region='Unknown'
-for(i in 1:nrow(s)) {
-  #split the string for the route
-  route=str_split(s[i,1],',')
-  s[i,2]=filter(supermarket,Store==route[[1]][1])$Region
-}
+s=regionClassifier(routeFilename = 'data/SaturdayRouteStores.txt',storeDf = saturday)
 
 #Put each route region into a new data frame
 North_route=filter(w,Region=='North')
@@ -162,8 +86,6 @@ North_raster=raster(akl_North)
 North_raster=writeRaster(North_raster,'Shape_files/North.tif',format='GTiff',overwrite=TRUE)
 #If the file is available in Shape_files, run this command only
 North_raster=raster('Shape_files/North.tif')
-#Plot the map of the Northern region and the supermarkets
-tm_shape(North_raster)+tm_rgb()+tm_shape(North)+tm_dots(col='Type',palette='Dark2',size=0.3)
 
 #Save the South shape as a raster for reusability
 akl_South=openmap(as.numeric(st_bbox(South_shape))[c(4,1)],as.numeric(st_bbox(South_shape))[c(2,3)],type='osm')
@@ -198,8 +120,15 @@ Central_raster=raster(akl_Central)
 Central_raster=writeRaster(Central_raster,'Shape_files/Central.tif',format='GTiff')
 #If the file is saved in Shape_files,run this command only
 Central_raster=raster('Shape_files/Central.tif')
+#Create the tif file for Auckland map for reusability
+akl_map=openmap(as.numeric(st_bbox(akl_shape1))[c(4,1)],as.numeric(st_bbox(akl_shape1))[c(2,3)],type='osm')
+akl_map=openproj(akl_map)
+akl_raster=raster(akl_map)
+akl_raster=writeRaster(akl_raster,'Shape_files/Auckland.tif',format='GTiff')
+#If the tif file is available, run this command only
+akl_raster=raster('Shape_files/Auckland.tif')
 
-source('regionalRoute.R')
+#Plot the map for each region
 North_map=regionalRoute(regional_raster= North_raster,regional_trips = North_route,regional_sat_trips = North_sat_route,region_stores = North)
 tmap_save(North_map,'Plot/North_routes.png',width = 2100,height=1200)
 South_map=regionalRoute(regional_raster= South_raster,regional_trips = South_route,regional_sat_trips = South_sat_route,region_stores = South)
@@ -210,3 +139,5 @@ West_map=regionalRoute(regional_raster= West_raster,regional_trips = West_route,
 tmap_save(West_map,'Plot/West_routes.png',width = 2500,height=850)
 Central_map=regionalRoute(regional_raster= Central_raster,regional_trips = Central_route,regional_sat_trips = Central_sat_route,region_stores = Central)
 tmap_save(Central_map,'Plot/Central_routes.png',width = 2500,height=900)
+Auckland_map=regionalRoute(regional_raster= akl_raster,regional_trips = w,regional_sat_trips = s,region_stores = supermarket1)
+tmap_save(Auckland_map,'Plot/Auckland_routes.png',width=2500,height=1100)
--- GeospatialFunctions.R
+++ GeospatialFunctions.R
@@ -1,3 +1,59 @@
+arrowRoute=function(routeFilename,storeDf) {
+  #Identify which kind of map should be shown and put on label
+  if(identical(storeDf,supermarket)) {
+    name='Weekday '
+  } else {
+    name='Saturday '
+  }
+  #Read the route file
+  usedRoute=readLines(routeFilename)
+  #Create matrix to store the route file
+  tripMatrix=matrix(0,nrow(storeDf)-1,4)
+  colnames(tripMatrix)=c('Long','Lat','LongEnd','LatEnd')
+  #Initialise node reach(the row for the matrix)
+  nodeReach=1
+  for(i in 1:length(usedRoute)) {
+    #Initialise start point and split the string
+    route=as.vector(st_coordinates(filter(storeDf,Store=='Distribution Centre Auckland')$geometry))
+    used=strsplit(usedRoute[i],split=', ')
+    for(j in 1:length(used[[1]])) {
+      #Add coordinate to the start and the end point of the arrows
+      tripMatrix[,'Long'][nodeReach]=route[1]
+      tripMatrix[,'Lat'][nodeReach]=route[2]
+      #Add coordinate to the end point of arrows
+      route1=as.vector(st_coordinates(filter(storeDf,Store==used[[1]][j])$geometry))
+      tripMatrix[,'LongEnd'][nodeReach]=route1[1]
+      tripMatrix[,'LatEnd'][nodeReach]=route1[2]
+      route=route1
+      nodeReach=nodeReach+1
+    }
+    
+  }
+  #change tripMatrix to a dataframe
+  tripMatrix=as.data.frame(tripMatrix)
+  #Return the map
+  map=ggplot(akl_shape)+
+    geom_sf(data=akl_shape,fill='grey')+
+    geom_sf(data=storeDf,aes(color=Type))+
+    scale_color_brewer(palette="Dark2")+
+    geom_segment(data=tripMatrix,aes(x=Long,y=Lat,xend=LongEnd,yend=LatEnd),
+                 arrow=arrow(length=unit(0.2,'cm')),alpha=0.3)+
+    labs(title=paste0(name, 'Routes'))
+  return(map)
+}
+regionClassifier=function(routeFilename,storeDf) {
+  #Read the route file
+  usedRoute=readLines(routeFilename)
+  usedRoute=as.data.frame(usedRoute)
+  colnames(usedRoute)='Route'
+  usedRoute$Region='Unknown'
+  for(i in 1:nrow(usedRoute)) {
+    #split the string for the route
+    route=str_split(usedRoute[i,1],', ')
+    usedRoute[i,2]=filter(storeDf,Store==route[[1]][1])$Region
+  }
+  return(usedRoute)
+}
 regionalRoute=function (regional_raster,regional_trips,regional_sat_trips,region_stores) {
   #This function plot regional routes for trips to Woolworth stores
   Sat_stores=filter(region_stores,Type%in%c('Countdown','Distribution Centre'))
@@ -14,7 +70,7 @@ regionalRoute=function (regional_raster,regional_trips,regional_sat_trips,region
   #Get the route to each store in each route of the region, including the route back
   for(i in 1:nrow(regional_trips)) {
     #Split the route into set of Strings
-    route=str_split(regional_trips[i,1],',')
+    route=str_split(regional_trips[i,1],', ')
     #Draw the route for each individual trip
     for (j in 1:length(route[[1]])) {
       if(j==1) {
@@ -35,7 +91,7 @@ regionalRoute=function (regional_raster,regional_trips,regional_sat_trips,region
   }
   for(i in 1:nrow(regional_sat_trips)) {
     #Split the route into set of Strings
-    route=str_split(regional_sat_trips[i,1],',')
+    route=str_split(regional_sat_trips[i,1],', ')
     #Draw the route for each individual trip
     for (j in 1:length(route[[1]])) {
       if(j==1) {
